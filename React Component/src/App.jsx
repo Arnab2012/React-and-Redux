d6ce1ae9f82eb530cdc621c3d6ab17e4a9018588
ARNAB BASAK <basakarnab912@gmail.com>
2024-11-21 20:24:51 +0530

    Added Bootstrap Code

--- React Component/src/App.jsx
+++ React Component/src/App.jsx
@@ -1,17 +1,37 @@
 import Dynamic from "./components/Dynamic";
 import MyButton from "./components/MyButton";
 import Random from "./components/Random";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <div>
-    <h1>This is my first React Component</h1>
-    <Dynamic></Dynamic>
-    <Random></Random>
-    <Random></Random>
-    <Random></Random>
-    <Random></Random>
-    <Random></Random>
-    <MyButton></MyButton>
-  </div>
+  return (
+    <div>
+      {/* including bootstrap card */}
+      <div className="card" style={{ width: "18rem" }}>
+        <img src="path-to-your-image.jpg" className="card-img-top" alt="Card image" />
+        <div className="card-body">
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+          </p>
+        </div>
+      </div>
+
+      <h1>This is my first React Component</h1>
+      
+      {/* including dynamic components */}
+      <Dynamic />
+
+      {/* including reusable components */}
+      <Random />
+      <Random />
+      <Random />
+      <Random />
+      <Random />
+
+      {/* including MyButton */}
+      <MyButton />
+    </div>
+  );
 }
+
 export default App;
