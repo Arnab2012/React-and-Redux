2e1570c63289adb0b50939031f2d98aa7e5ae13c
ARNAB BASAK <basakarnab912@gmail.com>
2024-11-21 11:44:09 +0530

    First Commit in React

---
+++ React Component/src/App.jsx
@@ -0,0 +1,9 @@
+import MyButton from "./components/MyButton";
+
+function App() {
+  return <div>
+    <h1>This is my first React Component</h1>
+    <MyButton></MyButton>
+  </div>
+}
+export default App;
---
+++ React Component/src/components/MyButton.jsx
@@ -0,0 +1,6 @@
+function MyButton(){
+    return <button>
+        Hit me
+    </button>
+}
+export default MyButton;
