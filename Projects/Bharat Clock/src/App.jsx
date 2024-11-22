5e8ad5456bd74a271445037cd4faa2a51436b27f
ARNAB BASAK <basakarnab912@gmail.com>
2024-11-22 19:47:51 +0530

    Added Project Bharat Clock

---
+++ Projects/Bharat Clock/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import CurrTime from './components/CurrTime';
+import Heading from './components/Heading';
+import Slogan from './components/Slogan';
+
+function App() { 
+
+  return <center>
+    <Heading></Heading>
+    <Slogan></Slogan>
+    <CurrTime></CurrTime>
+  </center>
+    
+}
+
+export default App
