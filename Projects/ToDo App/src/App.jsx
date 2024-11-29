45334e22f733c60dd26d865fc5bfddc4fe7a48a5
ARNAB BASAK <basakarnab912@gmail.com>
2024-11-29 19:24:38 +0530

    Updated ToDo App

--- Projects/ToDo App/src/App.jsx
+++ Projects/ToDo App/src/App.jsx
@@ -1,54 +1,35 @@
 import AppName from "./components/AppName";
 import Input from "./components/Input";
-// import Item1 from "./components/Item1";
-// import Item2 from "./components/Item2";
-
 import "./App.css";
 import Item from "./components/Item";
-import { useState } from "react";
 import EmptyMessage from "./components/EmptyMessage";
-import { ItemContext } from "./store/ItemStore";
+import ItemContextProvider from "./store/ItemStore";
+
 
 function App() {
-  // let todoItems = [
-  //   { todoName: "Buy Milk", todoDate: "10/4/2023" },
-  //   { todoName: "Go to College", todoDate: "10/4/2023" },
-  //   { todoName: "Go to Gym", todoDate: "10/4/2023" },
-  //   { todoName: "Go to Tution", todoDate: "10/8/2023" },
-  // ];
+  // using useState() here
+  // let [todoItems,setTodoItems]=useState([]);
 
-  let [todoItems,setTodoItems]=useState([]);
-
-  // when the add button is clicked add new item and pass to Item component
-  let addNewItem=(todoName,todoDate)=>{
-    setTodoItems([...todoItems,{todoName,todoDate}]);
-  }
-
-  // when the delete button is clicked remove the item and pass to Item component
-  let deleteItem=(todoName,todoDate)=>{
-    setTodoItems(todoItems.filter((i)=> !(i.todoName === todoName && i.todoDate === todoDate)));
-  }
-
+  // now everything will come from ItemContextProvider from ItemStore Component
   return (
     // using Context API here
     // we are passing values as object todoItems [],and two methods addNewItem and deleteItem
-    <ItemContext.Provider value={{todoItems,addNewItem,deleteItem}}>
-      
-    <center className="todoContainer">
-      <AppName></AppName>
+    <ItemContextProvider>
+      <center className="todoContainer">
+        <AppName></AppName>
 
-      {/* here we are passing as prop */}
+        {/* here we are passing as prop */}
 
-      {/* <Input onAddButtonClick={handleAddButtonClick}></Input> */}
-      {/* <EmptyMessage Items={todoItems}></EmptyMessage> */}
-      {/* <Item Items={todoItems} onDeleteButtonClick={handleDeleteButtonClick}></Item> */}
+        {/* <Input onAddButtonClick={handleAddButtonClick}></Input> */}
+        {/* <EmptyMessage Items={todoItems}></EmptyMessage> */}
+        {/* <Item Items={todoItems} onDeleteButtonClick={handleDeleteButtonClick}></Item> */}
 
-      {/* here we are using from (Context Provider) ItemStore component */}
-      <Input/>
-      <EmptyMessage/>
-      <Item/>
-    </center>
-    </ItemContext.Provider>
+        {/* here we are using from (Context Provider) ItemStore component */}
+        <Input />
+        <EmptyMessage />
+        <Item />
+      </center>
+    </ItemContextProvider>
   );
 }
 
--- Projects/ToDo App/src/store/ItemStore.jsx
+++ Projects/ToDo App/src/store/ItemStore.jsx
@@ -1,4 +1,68 @@
+import { useReducer } from "react";
 import { createContext } from "react";
 
-export const ItemContext =createContext([]);
+export const ItemContext = createContext([]);
 
+// adding Business Logic (Item Addition and deletion logic here)
+
+// pure function for useReducer()
+const todoItemsReducer = (currentItems, action) => {
+  let newToDoItems = currentItems;
+  // for adding new item
+  if (action.type === "ADD_ITEM") {
+    newToDoItems = [
+      ...currentItems,
+      { todoName: action.payload.todoName, todoDate: action.payload.todoDate },
+    ];
+  }
+  // for deleting a item
+  else if (action.type === "DELETE_ITEM") {
+    newToDoItems = currentItems.filter(
+      (i) =>
+        !(
+          i.todoName === action.payload.todoName &&
+          i.todoDate === action.payload.todoDate
+        )
+    );
+  }
+  return newToDoItems;
+};
+
+const ItemContextProvider = ({ children }) => {
+  // using useReducer() here
+  const [todoItems, dispatchTodoItems] = useReducer(todoItemsReducer, []);
+
+  // when the add button is clicked add new item and pass to Item component
+  let addNewItem = (todoName, todoDate) => {
+    // creating a new action object
+    const addItemAction = {
+      type: "ADD_ITEM",
+      payload: {
+        todoName,
+        todoDate,
+      },
+    };
+    dispatchTodoItems(addItemAction);
+  };
+
+  // when the delete button is clicked remove the item and pass to Item component
+  let deleteItem = (todoName, todoDate) => {
+    // creating a new action object
+    const deleteItemAction = {
+      type: "DELETE_ITEM",
+      payload: {
+        todoName,
+        todoDate,
+      },
+    };
+    dispatchTodoItems(deleteItemAction);
+  };
+
+  return (
+    <ItemContext.Provider value={{ todoItems, addNewItem, deleteItem }}>
+      {children}
+    </ItemContext.Provider>
+  );
+};
+
+export default ItemContextProvider;
